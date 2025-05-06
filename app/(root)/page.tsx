19c89caa351d5b5086737a49c9f8385212c08b7d
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-05-06 21:46:19 +0600

    fix tailwind css and hero section done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
     <>
-      <h1>Welcome</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your startup,
+          <br />
+          connect with enterpreneurs
+        </h1>
+        <p className="sub-heading">
+          Submit Ideas, Vote on Pitches, and Get Notice in Virtual Competitions
+        </p>
+      </section>
     </>
   );
 }
