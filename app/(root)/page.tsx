5379b66a0364241fb32b2c69212a8fd02f6e38ff
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-05-06 22:28:52 +0600

    search form done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,22 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { query?: string };
+}) {
+  const query = searchParams.query ?? "";
   return (
-    <>
-      <section className="pink_container">
-        <h1 className="heading">
-          Pitch your startup,
-          <br />
-          connect with enterpreneurs
-        </h1>
-        <p className="sub-heading">
-          Submit Ideas, Vote on Pitches, and Get Notice in Virtual Competitions
-        </p>
-      </section>
-    </>
+    <section className="pink_container">
+      <h1 className="heading">
+        Pitch your startup,
+        <br />
+        connect with entrepreneurs
+      </h1>
+      <p className="sub-heading">
+        Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+      </p>
+      <SearchForm query={query} />
+    </section>
   );
 }
